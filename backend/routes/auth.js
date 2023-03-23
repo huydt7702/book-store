f31267b7880124cee92a95d69615e1b9efec2100
huydt7702 <developer.vanhuy@gmail.com>
2023-03-23 12:52:32 +0700

    Fix Error: Route.post() requires a callback function but got a [object Undefined]

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,13 +4,21 @@ const router = require("express").Router();
 const { verifyToken } = require("../controllers/verifyToken");
 
 //REGISTER
-router.post("/register", authController.registerUser);
+router.post("/register", function (req, res) {
+    authController.registerUser(req, res);
+});
 
 //REFRESH TOKEN
-router.post("/refresh", authController.requestRefreshToken);
+router.post("/refresh", function (req, res) {
+    authController.requestRefreshToken(req, res);
+});
 //LOG IN
-router.post("/login", authController.loginUser);
+router.post("/login", function (req, res) {
+    authController.loginUser(req, res);
+});
 //LOG OUT
-router.post("/logout", authController.logOut);
+router.post("/logout", function (req, res) {
+    authController.userLogout(req, res);
+});
 
 module.exports = router;
